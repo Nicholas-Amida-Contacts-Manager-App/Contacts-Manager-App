d1a113901880ce516a979a5dbfff8e006eb127e1
Nicholas Barrera <nickbarerra722@gmail.com>
2022-10-14 15:42:31 -0500

    update

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -18,28 +18,14 @@ public class ContactsApp {
     Contacts nico = new Contacts("Nico", "barrera", "4444444444");
     Contacts jordy = new Contacts("Jordy", "ostar", "3333333333");
 
-    public static int mainMenu () {
-        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
-        System.out.println(" 1. View contacts.");
-        System.out.println(" 2. Add a new contact.");
-        System.out.println(" 3. Search a contact by name.");
-        System.out.println(" 4. Delete an existing contact.");
-        System.out.println(" 5. Exit.");
+    public static void mainMenu () {
 
-        Scanner scanner = new Scanner(System.in);
-        int userInput = scanner.nextInt();
-        return userInput;
+
+
     }
 
     public static void addPerson () throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(" 2. Add a new contact");
-        System.out.println(" First Name: ");
-        String firstName = scanner.nextLine();
-        System.out.println(" Last Name: ");
-        String lastName = scanner.nextLine();
-        System.out.println(" Phone Number: ");
-        String number = scanner.nextLine();
+
 
         Files.write(
                 Paths.get("data", "contacts.txt"),
